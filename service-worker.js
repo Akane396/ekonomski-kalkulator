d462c2e1d93ebf8878a131cbbd3bcbc7032003f0
Akane396 <adelic427@gmail.com>
2026-03-27 13:29:46 +0100

    update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'Kalkulator-app-v4.9.6'; // Promijeni u v3, v4... kad god nešto mijenjaš na stranici
+const CACHE_NAME = 'Kalkulator-app-v4.9.8'; // Promijeni u v3, v4... kad god nešto mijenjaš na stranici
 const urlsToCache = [
   'index.html',
   'kalkulacije.css',
